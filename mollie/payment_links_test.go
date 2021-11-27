fdf2d7603d6211f1503f35825b39ea324cd32797
Victor H. Avelar <aovo128446@upemor.edu.mx>
2021-11-27 16:12:40 +0100

    Migrate payment_links.go tests to testify

--- mollie/payment_links_test.go
+++ mollie/payment_links_test.go
@@ -2,92 +2,321 @@ package mollie
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/VictorAvelar/mollie-api-go/v3/testdata"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestPaymentLinkService_Get(t *testing.T) {
-	setup()
-	defer teardown()
-	_ = tClient.WithAuthenticationValue("test_token")
-	tMux.HandleFunc("/v2/payment-links/test_id", func(w http.ResponseWriter, r *http.Request) {
-		testHeader(t, r, AuthHeader, "Bearer test_token")
-		testMethod(t, r, "GET")
-		if _, ok := r.Header[AuthHeader]; !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-		}
+type paymentLinksSuite struct{ suite.Suite }
 
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(testdata.GetPaymentLinkResponse))
-	})
+func (ps *paymentLinksSuite) SetupSuite() { setEnv() }
 
-	v, err := tClient.PaymentLinks.Get(context.TODO(), "test_id")
-	if err != nil {
-		t.Fatal(err)
+func (ps *paymentLinksSuite) TearDownSuite() { unsetEnv() }
+
+func (ps *paymentLinksSuite) TestPaymentLinkService_Get() {
+	type args struct {
+		ctx         context.Context
+		paymentLink string
+		opts        *PaymentLinkOptions
 	}
 
-	if v.Amount.Value != "24.95" {
-		t.Error("unexpected response")
-	}
-}
+	cases := []struct {
+		name    string
+		args    args
+		wantErr bool
+		err     error
+		pre     func()
+		handler http.HandlerFunc
+	}{
+		{
+			"get payment links works as expected.",
+			args{
+				context.Background(),
+				"pl_ka21123129",
+				nil,
+			},
+			false,
+			nil,
+			noPre,
+			func(w http.ResponseWriter, r *http.Request) {
+				testHeader(ps.T(), r, AuthHeader, "Bearer token_X12b31ggg23")
+				testMethod(ps.T(), r, "GET")
+				testQuery(ps.T(), r, "testmode=true")
 
-func TestPaymentLinkService_Create(t *testing.T) {
-	setup()
-	defer teardown()
-	_ = tClient.WithAuthenticationValue("test_token")
-	tMux.HandleFunc("/v2/payment-links", func(w http.ResponseWriter, r *http.Request) {
-		testHeader(t, r, AuthHeader, "Bearer test_token")
-		testMethod(t, r, "POST")
-		if _, ok := r.Header[AuthHeader]; !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-		}
-
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(testdata.CreatePaymentLinkResponse))
-	})
-
-	v, err := tClient.PaymentLinks.Create(
-		context.TODO(),
-		PaymentLink{
-			Description: "payment_test_desc",
+				if _, ok := r.Header[AuthHeader]; !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+				}
+				_, _ = w.Write([]byte(testdata.GetPaymentLinkResponse))
+			},
 		},
-		nil,
-	)
-	if err != nil {
-		t.Fatal(err)
+		{
+			"get payment links, an error is returned from the server",
+			args{
+				context.Background(),
+				"pl_ka21123129",
+				nil,
+			},
+			true,
+			fmt.Errorf("response failed with status 500 Internal Server Error\npayload: "),
+			noPre,
+			errorHandler,
+		},
+		{
+			"get payment links, an error occurs when parsing json",
+			args{
+				context.Background(),
+				"pl_ka21123129",
+				nil,
+			},
+			true,
+			fmt.Errorf("invalid character 'h' looking for beginning of object key string"),
+			noPre,
+			encodingHandler,
+		},
+		{
+			"get payment links, invalid url when building request",
+			args{
+				context.Background(),
+				"pl_ka21123129",
+				nil,
+			},
+			true,
+			errBadBaseURL,
+			crashSrv,
+			errorHandler,
+		},
 	}
 
-	if v.ID != "pl_4Y0eZitmBnQ6IDoMqZQKh" {
-		t.Fail()
+	for _, c := range cases {
+		setup()
+		defer teardown()
+
+		ps.T().Run(c.name, func(t *testing.T) {
+			c.pre()
+			tMux.HandleFunc(fmt.Sprintf("/v2/payment-links/%s", c.args.paymentLink), c.handler)
+
+			m, err := tClient.PaymentLinks.Get(c.args.ctx, c.args.paymentLink)
+			if c.wantErr {
+				ps.NotNil(err)
+				ps.EqualError(err, c.err.Error())
+			} else {
+				ps.Nil(err)
+				ps.IsType(&PaymentLink{}, m)
+			}
+		})
 	}
 }
 
-func TestPaymentLinkService_List(t *testing.T) {
-	setup()
-	defer teardown()
-	_ = tClient.WithAuthenticationValue("test_token")
-	tMux.HandleFunc("/v2/payment-links", func(w http.ResponseWriter, r *http.Request) {
-		testHeader(t, r, AuthHeader, "Bearer test_token")
-		testMethod(t, r, "GET")
-		if _, ok := r.Header[AuthHeader]; !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-		}
-
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(testdata.ListPaymentLinksResponse))
-	})
-
-	v, err := tClient.PaymentLinks.List(
-		context.TODO(),
-		nil,
-	)
-	if err != nil {
-		t.Fatal(err)
+func (ps *paymentLinksSuite) TestPaymentLinkService_Create() {
+	type args struct {
+		ctx         context.Context
+		paymentLink PaymentLink
+		options     *PaymentLinkOptions
 	}
 
-	if v.Count != 1 {
-		t.Fail()
+	cases := []struct {
+		name    string
+		args    args
+		wantErr bool
+		err     error
+		pre     func()
+		handler http.HandlerFunc
+	}{
+		{
+			"create payment links works as expected.",
+			args{
+				context.Background(),
+				PaymentLink{
+					Amount: Amount{Value: "20.00", Currency: "EUR"},
+				},
+				&PaymentLinkOptions{ProfileID: "prf_12312312"},
+			},
+			false,
+			nil,
+			noPre,
+			func(w http.ResponseWriter, r *http.Request) {
+				testHeader(ps.T(), r, AuthHeader, "Bearer token_X12b31ggg23")
+				testMethod(ps.T(), r, "POST")
+				testQuery(ps.T(), r, "profileId=prf_12312312&testmode=true")
+
+				if _, ok := r.Header[AuthHeader]; !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+				}
+				_, _ = w.Write([]byte(testdata.CreatePaymentLinkResponse))
+			},
+		},
+		{
+			"create payment links, an error is returned from the server",
+			args{
+				context.Background(),
+				PaymentLink{},
+				nil,
+			},
+			true,
+			fmt.Errorf("response failed with status 500 Internal Server Error\npayload: "),
+			noPre,
+			errorHandler,
+		},
+		{
+			"create payment links, an error occurs when parsing json",
+			args{
+				context.Background(),
+				PaymentLink{},
+				nil,
+			},
+			true,
+			fmt.Errorf("invalid character 'h' looking for beginning of object key string"),
+			noPre,
+			encodingHandler,
+		},
+		{
+			"create payment links, invalid url when building request",
+			args{
+				context.Background(),
+				PaymentLink{},
+				nil,
+			},
+			true,
+			errBadBaseURL,
+			crashSrv,
+			errorHandler,
+		},
 	}
+
+	for _, c := range cases {
+		setup()
+		defer teardown()
+
+		ps.T().Run(c.name, func(t *testing.T) {
+			c.pre()
+			tMux.HandleFunc("/v2/payment-links", c.handler)
+
+			m, err := tClient.PaymentLinks.Create(c.args.ctx, c.args.paymentLink, c.args.options)
+			if c.wantErr {
+				ps.NotNil(err)
+				ps.EqualError(err, c.err.Error())
+			} else {
+				ps.Nil(err)
+				ps.IsType(&PaymentLink{}, m)
+			}
+		})
+	}
+}
+
+func (ps *paymentLinksSuite) TestPaymentLinkService_List() {
+	type args struct {
+		ctx  context.Context
+		opts *PaymentLinkOptions
+	}
+
+	cases := []struct {
+		name    string
+		args    args
+		wantErr bool
+		err     error
+		pre     func()
+		handler http.HandlerFunc
+	}{
+		{
+			"list payment links works as expected.",
+			args{
+				context.Background(),
+				nil,
+			},
+			false,
+			nil,
+			noPre,
+			func(w http.ResponseWriter, r *http.Request) {
+				testHeader(ps.T(), r, AuthHeader, "Bearer token_X12b31ggg23")
+				testMethod(ps.T(), r, "GET")
+				testQuery(ps.T(), r, "testmode=true")
+
+				if _, ok := r.Header[AuthHeader]; !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+				}
+				_, _ = w.Write([]byte(testdata.ListPaymentLinksResponse))
+			},
+		},
+		{
+			"list payment links with options works as expected.",
+			args{
+				context.Background(),
+				&PaymentLinkOptions{
+					ProfileID: "pfl_11211",
+				},
+			},
+			false,
+			nil,
+			noPre,
+			func(w http.ResponseWriter, r *http.Request) {
+				testHeader(ps.T(), r, AuthHeader, "Bearer token_X12b31ggg23")
+				testMethod(ps.T(), r, "GET")
+				testQuery(ps.T(), r, "profileId=pfl_11211&testmode=true")
+
+				if _, ok := r.Header[AuthHeader]; !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+				}
+				_, _ = w.Write([]byte(testdata.ListPaymentLinksResponse))
+			},
+		},
+		{
+			"list payment links, an error is returned from the server",
+			args{
+				context.Background(),
+				nil,
+			},
+			true,
+			fmt.Errorf("response failed with status 500 Internal Server Error\npayload: "),
+			noPre,
+			errorHandler,
+		},
+		{
+			"list payment links, an error occurs when parsing json",
+			args{
+				context.Background(),
+				nil,
+			},
+			true,
+			fmt.Errorf("invalid character 'h' looking for beginning of object key string"),
+			noPre,
+			encodingHandler,
+		},
+		{
+			"list payment links, invalid url when building request",
+			args{
+				context.Background(),
+				nil,
+			},
+			true,
+			errBadBaseURL,
+			crashSrv,
+			errorHandler,
+		},
+	}
+
+	for _, c := range cases {
+		setup()
+		defer teardown()
+
+		ps.T().Run(c.name, func(t *testing.T) {
+			c.pre()
+			tMux.HandleFunc("/v2/payment-links", c.handler)
+
+			m, err := tClient.PaymentLinks.List(c.args.ctx, c.args.opts)
+			if c.wantErr {
+				ps.NotNil(err)
+				ps.EqualError(err, c.err.Error())
+			} else {
+				ps.Nil(err)
+				ps.IsType(&PaymentLinksList{}, m)
+			}
+		})
+	}
+}
+
+func TestPaymentLinksService(t *testing.T) {
+	suite.Run(t, new(paymentLinksSuite))
 }
